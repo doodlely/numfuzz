1b8a350fa460c73ef212eacf2e698e961d889dc0
Ramon Torres <raymondjavaxx@gmail.com>
2013-02-01 15:06:25 +0800

    Separate lib tests from plugin tests

--- tests/library.js
+++ tests/library.js
@@ -28,15 +28,3 @@ test("Tests for NumFuzz.fuzzy() with invalid data", function() {
   strictEqual(NumFuzz.fuzzy(true), "NaN", "NumFuzz.fuzzy(true) returns 'NaN'");
   strictEqual(NumFuzz.fuzzy(false), "NaN", "NumFuzz.fuzzy(false) returns 'NaN'");
 });
-
-module("jQuery plugin", {setup: function() {
-  $('#qunit-fixture').append('<p><span class="number">1100</span> Followers</p>');
-}});
-
-test("Test for $.numFuzz()", function() {
-  $('#qunit-fixture .number').numFuzz();
-
-  expect(2);
-  strictEqual($('#qunit-fixture .number').text(), "1.1k", "Text should be equals to '1.1k'");
-  strictEqual($('#qunit-fixture .number').attr('title'), parseInt(1100, 10).toLocaleString(), "Title should be equals to number.toLocaleString()");
-});
